dea5e3250f11d4204e5f41684c5ef041a4cbc9c4
Canyonz <Faton64rus@yandex.ru>
2024-03-18 04:02:30 +0300

    #

--- src/entities/chartCard/ui/ChartCard.jsx
+++ src/entities/chartCard/ui/ChartCard.jsx
@@ -1,10 +1,10 @@
-import styles from './ChartCard.module.sass'
+import styles from "./ChartCard.module.sass";
 
 export const ChartCard = ({ title, description }) => {
-  return (
-    <div className={styles.chartCard}>
+	return (
+		<div className={styles.chartCard}>
 			<h3 className={styles.title}>{title}</h3>
 			<span className={styles.description}>{description}</span>
 		</div>
-  )
-}
+	);
+};
